08eb53d5632e53c381b14b7aaa48429738ebcfb8
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-16 17:35:37 +0600

    updated

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ async function run() {
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
+    //client connection should be open
   }
 }
 run().catch(console.dir);
