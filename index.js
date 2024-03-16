eecaf4615d365babfbda26ec8b8eda0b81748888
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-16 14:52:44 +0600

    booking data collection created

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const serviceCollection = client.db("carDoctor").collection("services");
+    const serviceCollection = client.db("carDoctor").collection("bookings");
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
