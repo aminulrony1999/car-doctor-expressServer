ef6c05fe5022d406e9f7ce4b12ce6b2d931af1e5
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-21 01:27:57 +0600

    bug fixed

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function run() {
     })
     app.patch('/bookings/:id',async(req,res)=>{
       const id = req.params.id;
-      const fileter = {_id : new ObjectId(id)};
+      const filter = {_id : new ObjectId(id)};
       const updatedBooking = req.body;
       console.log(updatedBooking);
       const updatedDoc = {
