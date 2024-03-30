f5f0a986077b5a2be9e0774ff0ef6df1af42e172
aminulrony1999 <aminulrony1999@gmail.com>
2024-03-30 17:50:49 +0600

    bug fixed

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-//middleware
-//fetch request won't work if middleware cors is not used
+
+// middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
+
+console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cardoctor.e9ybvtn.mongodb.net/?retryWrites=true&w=majority&appName=carDoctor`;
 
@@ -28,100 +31,116 @@ const client = new MongoClient(uri, {
   },
 });
 
-//our middleware
+// middlewares
 const logger = async (req, res, next) => {
   console.log("called:", req.host, req.originalUrl);
   next();
 };
 
-//middleware for verifying token
 const verifyToken = async (req, res, next) => {
-  console.log('Request headers',req.headers);
   const token = req.cookies?.token;
-  console.log("Value of token in middleware : ", token);
   if (!token) {
-    return res
-      .status(401)
-      .send({ message: "Forbidden, you are not authorised" });
+    return res.status(401).send({ message: "unauthorized access" });
   }
-  next();
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
 };
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
     const serviceCollection = client.db("carDoctor").collection("services");
     const bookingCollection = client.db("carDoctor").collection("bookings");
-    //auth related api
+
+    // auth related api
     app.post("/jwt", logger, async (req, res) => {
       const user = req.body;
-      //to generate secret, use the below  code to generate a random number
-      //write node in your terminal, it will open the node environment
-      //write the below code
-      //require('crypto').randomBytes(64).toString('hex')
-      //the above code will generate an random number
-      //the secret number is stored in .env file
+      console.log(user);
+
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
-      res.cookie("token", token, {
-        httpOnly: true,
-        secure: false,
-        sameSite: "none",
-      });
-      res.send({ success: true });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
     });
-    //seervice related api
+
+    // services related api
     app.get("/services", logger, async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
+
       const options = {
-        projection: { title: 1, price: 1, servie_id: 1, img: 1 },
-        //by using options , we can only collect data whatever we needed
-        //if value is 1, the info will be fetched
-        //if we put value 0, the specific data will be ignored
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
       };
+
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
     });
-    //fetching data from booking
+
+    // bookings
     app.get("/bookings", logger, verifyToken, async (req, res) => {
       console.log(req.query.email);
-      // console.log("token is : ", req.cookies.token);
+      console.log("ttttt token", req.cookies.token);
+      console.log("user in the valid token", req.user);
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+
       let query = {};
-      if (req.query?.email) query = { email: req.query.email };
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
       const result = await bookingCollection.find(query).toArray();
       res.send(result);
     });
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
+      console.log(booking);
       const result = await bookingCollection.insertOne(booking);
       res.send(result);
     });
+
     app.patch("/bookings/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedBooking = req.body;
       console.log(updatedBooking);
-      const updatedDoc = {
+      const updateDoc = {
         $set: {
           status: updatedBooking.status,
         },
       };
-      const result = await bookingCollection.updateOne(filter, updatedDoc);
+      const result = await bookingCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
+
     app.delete("/bookings/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await bookingCollection.deleteOne(query);
       res.send(result);
     });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -130,14 +149,14 @@ async function run() {
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
-    //client connection should be open
   }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Car doctor server is running");
+  res.send("doctor is running");
 });
+
 app.listen(port, () => {
-  console.log(`Car Doctor server is running on port ${port}`);
+  console.log(`Car Doctor Server is running on port ${port}`);
 });
